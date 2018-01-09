ad3eedc9bf6110433a43dfd20d52ab78ca169985
Margarita Timonina <margarita.timonina@gmail.com>
2018-01-09 21:14:13 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,5 +1,5 @@
 begin
  "#{greeting}"
- greeting = get_variable_from_file (./variable.rb, "greeting")
+ greeting = get_variable_from_file (''./variable.rb', "greeting")
  "greeting" = "Hello World"
  end
